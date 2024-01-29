808cb5577ae226d7410dff9856dc86c269470040
unknown <upendrapanchali83@gmail.com>
2024-01-29 14:49:09 +0530

    every thing uptodate

--- src/Components/Manageexpense/Viewexpense/Viewexpense.js
+++ src/Components/Manageexpense/Viewexpense/Viewexpense.js
@@ -5,6 +5,9 @@ import PersonIcon from '@mui/icons-material/Person';
 import MenuIcon from '@mui/icons-material/Menu';
 import Sidenav from "../../Sidenav/Sidenav";
 import MyContext from "../../../MyContext";
+import DeleteOutlineRoundedIcon from '@mui/icons-material/DeleteOutlineRounded';
+// import VisibilityIcon from '@mui/icons-material/Visibility';
+// import EditIcon from '@mui/icons-material/Edit';
 function Viewexpense(){
     const sharedvalue = useContext(MyContext);
     //code only for toggle the menu bar
@@ -76,6 +79,120 @@ function Viewexpense(){
                                             <th>action</th>
                                         </tr>
                                     </thead>
+                                    <tbody>
+                                        {
+                                            sharedvalue.expenseskeys.length>0 && sharedvalue.workerskeys.length>0 &&
+                                            sharedvalue.expenseskeys.map((expense,idx)=>(
+                                                <tr key={idx}>
+                                                    {/* 1 expense ID */}
+                                                    <td >
+                                                        <p className="view-manager-list-name">
+                                                            {expense}
+                                                        </p>
+                                                    </td>
+                                                    {/* 2 from date/time/place */}
+                                                    <td >
+                                                        <p className="view-manager-list-name">
+                                                            {sharedvalue.expensesdata[expense].fromdate}
+                                                        </p>
+                                                        <p className="view-manager-list-name">
+                                                            {sharedvalue.expensesdata[expense].fromtime}
+                                                        </p>
+                                                        <p className="view-manager-list-name">
+                                                            {sharedvalue.expensesdata[expense].fromplace}
+                                                        </p>
+                                                    </td>
+                                                    {/* 3 to date/time/place */}
+                                                    <td >
+                                                        <p className="view-manager-list-name">
+                                                            {sharedvalue.expensesdata[expense].todate}
+                                                        </p>
+                                                        <p className="view-manager-list-name">
+                                                            {sharedvalue.expensesdata[expense].totime}
+                                                        </p>
+                                                        <p className="view-manager-list-name">
+                                                            {sharedvalue.expensesdata[expense].toplace}
+                                                        </p>
+                                                    </td>
+                                                    {/* 4 mode */}
+                                                    <td >
+                                                        <p className="view-manager-list-name">
+                                                            {sharedvalue.expensesdata[expense].expmode}
+                                                        </p>
+                                                    </td>
+                                                    {/* 5 transport cost */}
+                                                    <td >
+                                                        <p className="view-manager-list-name">
+                                                            {Number(sharedvalue.expensesdata[expense].exptransportcost)}
+                                                        </p>
+                                                    </td>
+                                                    {/* foodcost */}
+                                                    <td >
+                                                        <p className="view-manager-list-name">
+                                                            {Number(sharedvalue.expensesdata[expense].expfoodcost)}
+                                                        </p>
+                                                    </td>
+                                                    {/* purpose */}
+                                                    <td >
+                                                        <p className="view-manager-list-name">
+                                                            {sharedvalue.expensesdata[expense].exppurpose}
+                                                        </p>
+                                                    </td>
+                                                    {/* customer name */}
+                                                    <td >
+                                                        <p className="view-manager-list-name">
+                                                            {sharedvalue.expensesdata[expense].expcustomername}
+                                                        </p>
+                                                    </td>
+                                                    {/* remark */}
+                                                    <td>
+                                                        {/* <div className='view-manager-list-acttion-icon'>
+                                                            <a href={sharedvalue.expensesdata[expense].fileurl} rel="noreferrer" target="_blank">
+                                                            <VisibilityIcon sx={{color:'#1A73E8',cursor:'pointer'}} fontSize="small"/>
+                                                            </a>
+                                                        </div> */}
+                                                        <p className="view-manager-list-name">
+                                                            {sharedvalue.expensesdata[expense].expremarks}
+                                                        </p>
+                                                    </td>
+                                                    {/* amount paid */}
+                                                    <td >
+                                                        <p className="view-manager-list-name">
+                                                            {Number(sharedvalue.expensesdata[expense].expamountpaid)}
+                                                        </p>
+                                                    </td>
+                                                    {/* amount pending */}
+                                                    <td >
+                                                        <p className="view-manager-list-name">
+                                                            {Number(sharedvalue.expensesdata[expense].expamountpending)}
+                                                        </p>
+                                                    </td>
+                                                     {/* final amount */}
+                                                     <td >
+                                                        <p className="view-manager-list-role">
+                                                            {Number(sharedvalue.expensesdata[expense].expamountpending)}
+                                                        </p>
+                                                    </td>
+                                                     {/* date added */}
+                                                     <td >
+                                                        <p className="view-manager-list-name">
+                                                            {sharedvalue.expensesdata[expense].expaddeddate}
+                                                        </p>
+                                                    </td>
+                                                     {/* action */}
+                                                     <td>
+                                                        <div className='view-manager-list-acttion-icon'>
+                                                            {/* <EditIcon sx={{color:'green',cursor:'pointer'}} fontSize="small" />
+                                                            <VisibilityIcon sx={{color:'#1A73E8',cursor:'pointer'}} fontSize="small" /> */}
+                                                            <DeleteOutlineRoundedIcon sx={{color:'red',cursor:'pointer'}} fontSize="small"
+                                                            
+                                                            />
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            ))
+                                        }
+                                    </tbody>
                                 </table>
                             </div>
                          </div>
