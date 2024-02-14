0f6aee08a8c35e1a444cb783130e1a8f237ae4bd
unknown <upendrapanchali83@gmail.com>
2024-02-14 16:08:06 +0530

    quotation flow completed

--- src/Components/Managequotation/Createquotation/Createquotation.js
+++ src/Components/Managequotation/Createquotation/Createquotation.js
@@ -336,9 +336,13 @@ function Createquotation(){
                                         <option value='5'>5</option>
                                         <option value='6'>6</option>
                                         <option value='7'>7</option>
-                                        <option value='7'>8</option>
-                                        <option value='7'>9</option>
-                                        <option value='7'>10</option>
+                                        {(quotinfo.quotmachinetype==='ULTRA'||quotinfo.quotmachinetype==='FALCON') && <option value='8'>8</option>}
+                                        {(quotinfo.quotmachinetype==='ULTRA'||quotinfo.quotmachinetype==='FALCON') && <option value='9'>9</option>}
+                                        {(quotinfo.quotmachinetype==='ULTRA'||quotinfo.quotmachinetype==='FALCON') && <option value='10'>10</option>}
+                                        {(quotinfo.quotmachinetype==='ULTRA'||quotinfo.quotmachinetype==='FALCON') && <option value='11'>11</option>}
+                                        {(quotinfo.quotmachinetype==='ULTRA'||quotinfo.quotmachinetype==='FALCON') && <option value='12'>12</option>}
+                                        {(quotinfo.quotmachinetype==='ULTRA'||quotinfo.quotmachinetype==='FALCON') && <option value='13'>13</option>}
+                                        {(quotinfo.quotmachinetype==='ULTRA'||quotinfo.quotmachinetype==='FALCON') && <option value='14'>14</option>}
                                     </select>
                                 </div>
                                 {/* price */}
--- src/Components/Managequotation/Viewquotation/Viewquotation.js
+++ src/Components/Managequotation/Viewquotation/Viewquotation.js
@@ -17,12 +17,24 @@ import ShareIcon from '@mui/icons-material/Share';
 //download icon
 import DownloadIcon from '@mui/icons-material/Download';
 
+import DeleteOutlineRoundedIcon from '@mui/icons-material/DeleteOutlineRounded';
+import {writeBatch} from "firebase/firestore";
+import { db } from "../../../Firebase";
 
-
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import { createquotes } from "../../../Data/Docs";
+import { updateDoc, deleteField } from "firebase/firestore";
 function Viewquotation(){
     const sharedvalue = useContext(MyContext);
-
-
+    const batch = writeBatch(db);//get a new write batch
+     //backdrop loading toggle
+     const[showloading,setshowloading] = useState(false);
+     //deleting lead
+     const [workerdelete,setworkerdelete] = useState({
+        active:false,
+        quoteid:''
+    });
     //use navigator is important for now
     const navigate = useNavigate();
     // search bar input 
@@ -38,9 +50,49 @@ function Viewquotation(){
    
 
     //test the pdf ends here
+
+    //function closing the quotation starts here
+    async function handleclosequatation(){
+        setshowloading(true);
+        try{
+            if(workerdelete.quoteid!==''){
+                await batch.update(createquotes,{
+                    [workerdelete.quoteid]:{
+                        ...sharedvalue.quotesdata[workerdelete.quoteid],
+                        quotstatus:'closed'
+                    }
+                });
+                await batch.commit();//commit all baches
+                setworkerdelete(prev=>({
+                    ...prev,
+                    active:false,
+                    quoteid:''
+                }))
+            }
+        }catch(e){
+            console.log('you got an error while handling closing the quotation ',e);
+        }
+        setshowloading(false);
+    }
+    //function closing the quotation ends here
+
+    //function to delete the quotation starts here
+    async function handledeletequotation(quoteid){
+        setshowloading(true);
+        try{
+            await updateDoc(createquotes,{
+                [quoteid]:deleteField()
+            });
+            await batch.commit();
+        }catch(e){
+            console.log('you got an error while deleting the quotation',e);
+        }
+         setshowloading(false);
+    }
+    //function to delete the quotation ends here
     return(
         <>
-            <div className={`manlead-con`} >
+            <div className={`manlead-con ${workerdelete.active===true?'manlead-con-inactive':''}`} >
                 <Sidenav menutoggle={menutoggle} handlemenutoggle={handlemenutoggle}/>
                 <div className='manage-con-inner'>
 
@@ -133,7 +185,7 @@ function Viewquotation(){
                                                             {/* 6.chute */}
                                                             <td>
                                                                 <p className="view-manager-list-name">
-                                                                    {sharedvalue.leadsdata[sharedvalue.quotesdata[quote].quotlead].chutes}
+                                                                    {sharedvalue.quotesdata[quote].quotcap}
                                                                 </p>
                                                             </td>
                                                             {/* 7. price */}
@@ -190,10 +242,16 @@ function Viewquotation(){
                                                                 <p className='view-manager-list-acttion-icon'>
                                                                     { (sharedvalue.quotesdata[quote].quotstatus==='open' || sharedvalue.quotesdata[quote].quotstatus==='rework') && sharedvalue.uid===sharedvalue.quotesdata[quote].quotcreatedby && <EditIcon fontSize="small" sx={{color:'green',cursor:'pointer'}} onClick={()=>navigate(`/managequotation/updatequotation/${quote}`)} />}
                                                                     {sharedvalue.uid===sharedvalue.quotesdata[quote].quotcreatedby && sharedvalue.quotesdata[quote].quotstatus==='approved' && <ShareIcon sx={{color:'grey',cursor:'pointer'}}  fontSize="small"/>}
-                                                                    {sharedvalue.uid===sharedvalue.quotesdata[quote].quotcreatedby && sharedvalue.quotesdata[quote].quotstatus==='approved' && <PDFDownloadLink document={<Comaasrgb quoteid={quote}/>} fileName="dynamic.pdf">
+                                                                    {sharedvalue.uid===sharedvalue.quotesdata[quote].quotcreatedby && (sharedvalue.quotesdata[quote].quotstatus==='approved'||sharedvalue.quotesdata[quote].quotstatus==='closed' ) && <PDFDownloadLink document={<Comaasrgb quoteid={quote}/>} fileName="dynamic.pdf">
                                                                         <DownloadIcon sx={{color:'black',cursor:'pointer'}}  fontSize="small" />
                                                                     </PDFDownloadLink> }
+                                                                    {sharedvalue.uid===sharedvalue.quotesdata[quote].quotcreatedby && sharedvalue.quotesdata[quote].quotstatus==='approved' && <p className="close-quotation-btn" onClick={()=>setworkerdelete(prev=>({
+                                                                ...prev,
+                                                                active:true,
+                                                                quoteid:quote
+                                                            }))} >close</p>}
                                                                     {sharedvalue.quotesdata[quote].quotstatus!=='closed' && sharedvalue.role==='admin' && <VisibilityIcon sx={{color:'#1A73E8',cursor:'pointer'}}  fontSize="small" onClick={()=>navigate(`/managequotation/verifyquotation/${quote}`)}/>}
+                                                                    {sharedvalue.quotesdata[quote].quotstatus==='closed' && sharedvalue.role==='admin' && <DeleteOutlineRoundedIcon sx={{color:'red' , cursor:'pointer'}} fontSize="small" onClick={()=>handledeletequotation(quote)}/>}
                                                                 </p>
                                                             </td>
 
@@ -210,6 +268,25 @@ function Viewquotation(){
                     </div>
                 </div>
             </div>
+            {/* popup to delete an item */}
+            <div className={`view-manager-list-popup-delete ${workerdelete.active===true?'active-delete-popup':''}`}>
+                <p>Are You Sure You want to close the quotation <span>{workerdelete.quoteid?workerdelete.quoteid:''}</span></p>
+                <div>
+                    <button onClick={()=>handleclosequatation()}>Yes</button>
+                    <button onClick={()=>setworkerdelete(prev=>({
+                        ...prev,
+                        active:false,
+                        quoteid:''
+                    }))}>No</button>
+                </div>
+            </div>
+            {/* here ia the back drop */}
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={showloading}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
         </>
     );
 }
